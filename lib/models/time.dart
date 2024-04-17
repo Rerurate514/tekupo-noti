da07185ec6862eb94ea4c8c5698e35045f686bc5
Rerurate514 <rerurateyuto@gmail.com>
2024-04-17 19:47:59 +0900

    timeクラスの作成

---
+++ lib/models/time.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+
+class Time{
+  final int hour;
+  final int minute;
+
+  Time get before10minTime => _calcHourMin(Time(hour, minute - 10));
+  Time get after5minTime => _calcHourMin(Time(hour, minute + 5));
+
+  Time(
+    this.hour,
+    this.minute
+  );
+
+  Time _calcHourMin(Time time){
+    int calcedHour = time.hour;
+    int calcedMinute = time.minute;
+
+    if(calcedMinute >= 60){
+      calcedMinute = 0;
+      calcedHour++;
+    }
+
+    if(calcedMinute < 0){
+      calcedMinute = (60 + calcedMinute);
+      calcedHour--;
+    }
+
+    return Time(calcedHour, calcedMinute);
+  }
+}
+
+void main(){
+  test("time-test", () {
+    Time time11 = Time(10, 30);
+    Time time12 = time11.before10minTime;
+    Time time13 = time11.after5minTime;
+    expect(time12.minute, 20);
+    expect(time13.minute, 35);
+
+    Time time21 = Time(10, 0);
+    Time time22 = time21.before10minTime;
+    Time time23 = time21.after5minTime;
+    expect(time22.minute,  50);
+    expect(time23.minute, 5);
+
+    Time time31 = Time(10, 55);
+    Time time32 = time31.before10minTime;
+    Time time33 = time31.after5minTime;
+    expect(time32.minute, 45);
+    expect(time33.minute, 0);
+  });
+}
