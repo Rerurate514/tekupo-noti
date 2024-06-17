9fb0466c8aa4a0515ab1a3d1c1692aa59774a55b
Rerurate514 <rerurateyuto@gmail.com>
2024-06-17 19:17:52 +0900

    tzのinitialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tekupo_noti/logic/permissionManager.dart';
 import 'package:tekupo_noti/pages/choiceNoticeSchedule.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() {
+  tz.initializeTimeZones();
   runApp(const MyApp());
 }
 
